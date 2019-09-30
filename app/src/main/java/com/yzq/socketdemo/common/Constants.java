206e22de0a86355b76fb1c368f89c9fbde4dfe28
renMengYang <942685687@qq.com>
2019-09-30 11:22:24 +0800

    Initial commit

---
+++ app/src/main/java/com/yzq/socketdemo/common/Constants.java
@@ -0,0 +1,17 @@
+package com.yzq.socketdemo.common;
+
+/**
+ * Created by yzq on 2017/9/26.
+ * 存放一些常量
+ */
+
+public class Constants {
+
+    /*intent tag*/
+    public static final String INTENT_IP = "192.168.1.105";//ip
+    public static final String INTENT_PORT = "9999";//port(端口号)
+
+
+    /*EventBus  msg*/
+    public static final String CONNET_SUCCESS = "connectSucccess";
+}
